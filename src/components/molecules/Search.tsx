f0d93e10b701fee43d7af5b29e32ccb003aaac5a
ClarXo <cheonaru@gmail.com>
2022-04-20 11:30:07 +0900

    chore(Search): prop & log removed

--- src/components/molecules/Search.tsx
+++ src/components/molecules/Search.tsx
@@ -5,7 +5,7 @@ import { Layout } from '../../styles';
 import { Button, FormErrorMessage, Icon, Input } from '../atoms';
 
 const FormWrapper = styled.div`
-  margin-top: 16px;
+  margin-top: 8px;
   padding: 0 16px;
 `;
 const FormWithStyle = styled.form`
@@ -16,16 +16,12 @@ interface SearchForm {
   searchText: string;
 }
 
-const Search: (props: any) => JSX.Element = (props) => {
+const Search: () => JSX.Element = () => {
   const { register, handleSubmit, formState } = useForm<SearchForm>();
 
   const onValid = (formData: SearchForm) => {
     console.log(formData);
   };
-
-  useEffect(() => {
-    console.log(formState.errors);
-  }, [formState]);
 
   return (
     <FormWrapper>
